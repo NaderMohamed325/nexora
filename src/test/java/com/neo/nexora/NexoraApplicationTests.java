b5b28c83f9e7e3c32f19fd7e47fd05e68d84c1bc
NaderMohamed325 <nadoorclasher@gmail.com>
2026-03-01 21:06:37 +0200

    Add Maven Wrapper and initial test setup for NexoraApplication

---
+++ src/test/java/com/neo/nexora/NexoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo.nexora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
