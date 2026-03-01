10546a1c891d3a5a67c252a50ccb3bd1024a126a
NaderMohamed325 <nadoorclasher@gmail.com>
2026-03-01 21:06:10 +0200

    Initialize NexoraApplication with Spring Boot and scheduling support

---
+++ src/main/java/com/neo/nexora/NexoraApplication.java
@@ -0,0 +1,15 @@
+package com.neo.nexora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class NexoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexoraApplication.class, args);
+    }
+
+}
