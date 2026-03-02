026689aca8e8bdd0a51c0d1fc2b4be17545df7dc
NaderMohamed325 <nadoorclasher@gmail.com>
2026-03-02 12:50:25 +0200

    Extend `UserRepository` with custom query methods for enhanced user retrieval options

--- src/main/java/com/neo/nexora/repository/UserRepository.java
+++ src/main/java/com/neo/nexora/repository/UserRepository.java
@@ -2,14 +2,31 @@ package com.neo.nexora.repository;
 
 import com.neo.nexora.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
     Optional<User> findByEmail(String email);
+
     boolean existsByUsername(String username);
+
     boolean existsByEmail(String email);
+
+    List<User>findUsers();
+
+    @Query(value = "SELECT * FROM users where username ILIKE %:username%", nativeQuery = true)
+    List<User> findByUsernameLike(@Param("username") String username);
+
+    Optional<User> findUserById(Long id);
+
+
+
+
 }
