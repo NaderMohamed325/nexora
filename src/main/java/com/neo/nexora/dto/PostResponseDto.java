adbbf4d68825ba9fe815041b49854d6e73882cda
NaderMohamed325 <nadoorclasher@gmail.com>
2026-03-09 15:34:27 +0200

    Remove unused `authorId` field from `PostResponseDto`

--- src/main/java/com/neo/nexora/dto/PostResponseDto.java
+++ src/main/java/com/neo/nexora/dto/PostResponseDto.java
@@ -18,7 +18,6 @@ public class PostResponseDto {
     private Long id;
     private String title;
     private String content;
-    private Long authorId;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private List<String> media_urls;
